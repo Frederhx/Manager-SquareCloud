91c3de84afc1a5a98c6181c8f9594bc5e48ad2d0
Frederhx <fredericoceliacn@gmail.com>
2025-07-25 14:39:29 -0300

    ignore.bat

--- upload_github.bat
+++ upload_github.bat
@@ -1,21 +1,2 @@
-@echo off
-SET "REPO=https://github.com/Frederhx/Manager-SquareCloud.git"
+Ignore
 
-git config --global user.name "Frederhx"
-git config --global user.email "fredericoceliacn@gmail.com"
-
-echo Fazendo pull para sincronizar com o GitHub...
-git pull origin main --allow-unrelated-histories
-
-echo Adicionando arquivos...
-git add .
-
-echo Commitando as alterações...
-git commit -m "Atualizando arquivos locais"
-
-echo Enviando para o GitHub...
-git push origin main
-
-echo.
-echo ✅ Upload concluído com sincronização!
-pause
